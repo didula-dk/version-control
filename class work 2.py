283de11c09faa8d76f66c2741048f550ad2d5f48
didula-dk <rajithdidula23@gmail.com>
2025-03-03 10:51:48 +0400

    Commit

---
+++ class work 2.py
@@ -0,0 +1,5 @@
+ans = input("is ishan gay ")
+if ans.upper() == "yes":
+    print("We knew it")
+else:
+    print("i think ur the gay one here")
---
+++ class work1.py
@@ -0,0 +1,3 @@
+num_1= float(input('Enter a first number'))
+num_2= float(input('Enter a second number'))
+print(num_1 + num_2)
